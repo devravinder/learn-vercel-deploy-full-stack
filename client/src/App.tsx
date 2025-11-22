e77d632251f079f039be83d0c0e46dff01fda30c
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2025-11-22 11:05:39 +0530

    fix: remove unused

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const apiEndpoint = import.meta.env.VITE_API_URL || "https://jsonplaceholder.typicode.com/todos/1"
